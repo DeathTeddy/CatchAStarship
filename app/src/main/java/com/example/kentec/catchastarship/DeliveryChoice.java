2e9ada12c1809a4a5b060aeae8f097b881c089f5
DeathTeddy <snakekiller147@gmail.com>
2018-02-18 03:59:34 -0500

    First version

---
+++ app/src/main/java/com/example/kentec/catchastarship/DeliveryChoice.java
@@ -0,0 +1,13 @@
+package com.example.kentec.catchastarship;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeliveryChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_choice);
+    }
+}
---
+++ app/src/main/java/com/example/kentec/catchastarship/Login.java
@@ -0,0 +1,41 @@
+package com.example.kentec.catchastarship;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/*
+    At least two Activities.
+    Save at least one data point with a SharedPreference.
+    Send at least one data point between Activities using an Intent.
+    Read in input from the user using at least one of each:
+        Checkbox
+        TextView
+        Radio Button (use a ButtonGroup)
+    Have at least on image in the application.
+    Include a BroadcastReceiver to deal with a system event.
+        Notify the user of the event (a Toast is fine but it is up to you).
+    One Activity needs a ConstraintLayout
+    One Activity needs a LinearLayout (horizontal).
+*/
+
+
+public class Login extends AppCompatActivity {
+
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private EditText mName, mPassword;
+    private Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
